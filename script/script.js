bd77f9544b1aac98c77d41247fdea675eb9b663c
Longoria48 <longoriaeagles36@yahoo.com>
2025-04-06 15:49:44 -0500

    Exception handling on form

--- script/script.js
+++ script/script.js
@@ -1,23 +1,46 @@
 //adding a h2 greeting heading to the customer
-
 //declaring a const variable to hold the current time
 const time = new Date().getHours();
-
 //declare greeting variable to update depending on the conditional statement
 let greeting = "";
 
 //will check the time and display the corresponding statement to the end-user
 if (time < 11)
 {
-  greeting = "hopes you are having a great morning";
+  greeting = "hopes you are having a swimming morning!";
 }
 else if (time < 20) 
 {
-  greeting = "hopes you are having a great day";
+  greeting = "hopes you are having a ta-ruffic day!";
 }
 else
 {
-  greeting = "hopes you have a great evening";
+  greeting = "hopes you have a paw-some evening!";
 }
 //display the greeting element in the h2 display element added into the header
 document.getElementById("greeting").innerHTML = greeting;
+
+//Exception Handling-contact form if numbers are entered into name input boxes
+function validateName()
+{
+  let firstName = document.getElementById("myFirstName").value;
+  let lastName = document.getElementById("myLastName").value;
+  let digits = /[0-9]/; //found on w3schools href=https://www.w3schools.com/jsref/jsref_regexp_digit.asp
+
+  if(digits.test(firstName)) //The test() method tests for a match in a string. https://www.w3schools.com/jsref/jsref_regexp_test.asp
+  {
+    document.getElementById("myFirstName").value = "Cannot contain numbers";
+    return false; // stop submission
+  }
+  else if(digits.test(lastName))
+  {
+    document.getElementById("myLastName").value = "Cannot contain numbers";
+    return false;
+  }
+  else
+  {
+    return true; //return true and continue
+  }
+}
+document.getElementById("myFirstName").addEventListener("input",validateName);
+document.getElementById("myLastName").addEventListener("input",validateName);
